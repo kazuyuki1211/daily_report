e69b9f078b7605aba2f39903a6d27bdbe05a1feb
miyazawa <56.-.ri.-.56@au.com>
2021-04-16 18:16:47 +0900

    Add Employee index

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -6,7 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import models.Employee;
-import util.DBUtil;
+import utils.DBUtil;
 
 public class EmployeeValidator {
     public static List<String> validate(Employee e, Boolean codeDuplicateCheckFlag, Boolean passwordCheckFlag){
